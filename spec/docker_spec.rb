bff84d51d61e11d184d70cc4529d87662b6c6166
Jason Hee <jasonheecs@gmail.com>
2019-01-01 05:21:01 +0800

    Fix code style based on linter (#2)

--- spec/docker_spec.rb
+++ spec/docker_spec.rb
@@ -1,25 +1,23 @@
 require 'spec_helper'
 
-describe "Dockerfile" do
+describe 'Dockerfile' do
 
   image = Docker::Image.build_from_dir('.')
   set :docker_image, image.id
 
-  it "should have the maintainer label" do
-    expect(image.json["Config"]["Labels"].has_key?("maintainer")).to be true
+  it 'should have the maintainer label' do
+    expect(image.json['Config']['Labels'].key?('maintainer')).to be true
   end
 
-  [
-    'bash',
-    'bash-completion',
-    'util-linux',
-    'coreutils',
-    'binutils',
-    'findutils',
-    'grep',
-    'build-base'
-  ].each do |packageName|
-    describe package(packageName) do
+  %w[bash
+     bash-completion
+     util-linux
+     coreutils
+     binutils
+     findutils
+     grep
+     build-base].each do |package|
+    describe package(package) do
       it { should be_installed }
     end
   end
